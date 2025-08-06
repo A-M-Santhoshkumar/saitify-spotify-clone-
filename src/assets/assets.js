1262e9e0c00793c78783a3087a3c836911843ccb
Santhoshkumar <a.m.santhoshkumar02@gmail.com>
2025-08-06 18:33:00 +0530

    song set fastly

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -24,33 +24,6 @@ import spotify_logo from "./spotify_logo.png";
 import clock_icon from "./clock_icon.png";
 
 
-import aadukalam from "./img/song-thumb/aadukalam.webp";
-import david from "./img/song-thumb/david.png";
-import dhaamdhoom from "./img/song-thumb/dhaam_dhoom.png";
-import enpt from "./img/song-thumb/enpt.jpeg";
-import guru from "./img/song-thumb/guru.png";
-import kodi from "./img/song-thumb/kodi.png";
-import manithan from "./img/song-thumb/manithan.png";
-import oh_manapenne from "./img/song-thumb/oh-manapennae.png";
-
-import tensromance from "./img/10s-romance-tamil.jpg";
-import arrmix from "./img/arr-mix.jpg";
-import hiphopradio from "./img/hip-hop-radio.jpg";
-import kollychill from "./img/kolly-chill-out.jpg";
-import munpanikaadhal from "./img/munpani-kaadhal.jpg";
-import purelykaadhal from "./img/purely-kaadhal.jpg";
-import romanticanirudh from "./img/romantic-anirudh.jpg";
-import yuvan from "./img/yuvan.jpg";
-
-import aaruyire from "./songs/Aaruyire.mp3";
-import naanpilaipeno from "./songs/Naan-Pizhaippeno.mp3";
-import kanavekanavae from "./songs/Kanave Kanave.mp3";
-import othasollala from "./songs/Otha-Sollaala.mp3";
-import anbeenanbe from "./songs/Anbe-En-Anbe.mp3";
-import poivalva from "./songs/Poi-Vazhva.mp3";
-import sirukkivaasasm from "./songs/Sirukki-Vaasam.mp3";
-import bodhaikaname from "./songs/Bodhai-Kaname.mp3";
-
 export const assets = {
   bell_icon,
   home_icon,
@@ -82,56 +55,56 @@ export const albumsData = [
   {
     id: 0,
     name: "Munpani Kaadhal",
-    image: munpanikaadhal,
+    image: "/img/munpani-kaadhal.jpg",
     desc: "Your weekly update of the most played tracks",
     bgColor: "#D10000",
   },
   {
     id: 1,
     name: "10s Romance Tamil",
-    image: tensromance,
+    image: "/img/10s-romance-tamil.jpg",
     desc: "Best Love Songs of 2010s from kollywood",
     bgColor: "#bcc45e",
   },
   {
     id: 2,
     name: "A.R. Rahman",
-    image: arrmix,
+    image: "/img/arr-mix.jpg",
     desc: "Hits of A.R. Rahman that will kill you",
     bgColor: "#029ed6",
   },
   {
     id: 3,
     name: "Kollywood Chill",
-    image: kollychill,
+    image: "/img/kolly-chill-out.jpg",
     desc: "Cool tracks from Kollywood music industry",
     bgColor: "#44337a",
   },
   {
     id: 4,
     name: "Purely Kadhal",
-    image: purelykaadhal,
+    image: "/img/purely-kaadhal.jpg",
     desc: "All the mushy feels from top romantic hits",
     bgColor: "#D10000",
   },
   {
     id: 5,
     name: "Romantic Anirudh",
-    image: romanticanirudh,
+    image: "/img/romantic-anirudh.jpg",
     desc: "Enjoy the Romantic side of Ani",
     bgColor: "#744210",
   },
   {
     id: 6,
     name: "Hip Hop Tamizha",
-    image: hiphopradio,
+    image: "/img/hip-hop-radio.jpg",
     desc: "Love and more love from Aadhi",
     bgColor: "#F7567C",
   },
   {
     id: 7,
     name: "Yuvan Hits",
-    image: yuvan,
+    image: "/img/yuvan.jpg",
     desc: "Love and more love from Aadhi",
     bgColor: "#4E3822",
   },
@@ -141,64 +114,64 @@ export const songsData = [
   {
     id: 0,
     name: "Otha Solala",
-    image: aadukalam,
-    file: othasollala,
+    image: "/img/song-thumb/aadukalam.webp",
+    file: "/songs/Otha-Sollaala.mp3",
     desc: "Happy tunes from Dhanush na!!",
     duration: "3:59",
   },
   {
     id: 1,
     name: "Kanave kanave",
-    image: david,
-    file: kanavekanavae,
+    image: "/img/song-thumb/david.png",
+    file: "/songs/Kanave Kanave.mp3",
     desc: "Dive into anirudh sad zone",
     duration: "4:44",
   },
   {
     id: 2,
     name: "Naan Pizhaipeno",
-    image: enpt,
-    file: naanpilaipeno,
+    image: "/img/song-thumb/enpt.jpeg",
+    file: "/songs/Naan-Pizhaippeno.mp3",
     desc: "Crazy Love from Dhanush",
     duration: "6:12",
   },
   {
     id: 3,
     name: "Aaruyire",
-    image: guru,
-    file: aaruyire,
+    image: "/img/song-thumb/guru.png",
+    file: "/songs/Aaruyire.mp3",
     desc: "Mesmerizing hits A.R.R > anything",
     duration: "5:10",
   },
   {
     id: 4,
     name: "Anbe en Anbe",
-    image: dhaamdhoom,
-    file: anbeenanbe,
+    image: "/img/song-thumb/dhaam_dhoom.png",
+    file: "/songs/Anbe-En-Anbe.mp3",
     desc: "Put a smile on your face with these happy tunes",
     duration: "5:40",
   },
   {
     id: 5,
     name: "Poi Vazhva",
-    image: manithan,
-    file: poivalva,
+    image: "/img/song-thumb/manithan.png",
+    file: "/songs/Poi-Vazhva.mp3",
     desc: "Dont ever feel down listen this",
     duration: "3:59",
   },
   {
     id: 6,
     name: "Sirukki Vaasam",
-    image: kodi,
-    file: sirukkivaasasm,
+    image: "/img/song-thumb/kodi.png",
+    file: "/songs/Sirukki-Vaasam.mp3",
     desc: "Put a smile on your face with these happy tunes",
     duration: "4:35",
   },
   {
     id: 7,
     name: "Bodhai Kaname",
-    image: oh_manapenne,
-    file: bodhaikaname,
+    image: "/img/song-thumb/oh-manapennae.png",
+    file: "/songs/Bodhai-Kaname.mp3",
     desc: "Put a smile on your face with these happy tunes",
     duration: "4:22",
   },
